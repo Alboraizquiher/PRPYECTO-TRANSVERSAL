a4c85c3c1ac30bb64309795f065fc2c99d73ca11
angelopozo <apozo2027@gmail.com>
2025-03-13 13:13:25 +0100

    added new navbar and improved usercontroller

--- LOGIN/PHP/UserController.php
+++ LOGIN/PHP/UserController.php
@@ -1,17 +1,36 @@
 <?php
+session_start();
+
+$user = new UserController();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if ($_POST['username'] == '' || $_POST['password'] == '') {
+        echo 'Username or password is empty';
+        header('Location: ../../login.html');
+    } else {
+        $user->username = $_POST['username'];
+        $user->password = $_POST['password'];
+    }
+
+    if (isset($_POST['login'])) {
+        $user->login();
+    } else if (isset($_POST['register'])) {
+        $user->register();
+    } else if (isset($_GET['logout'])) {
+        $user->logout();
+    }
+}
 class UserController
 {
-    // TODO: Preguntar José cómo llamar a los métodos de esta clase desde el front.
-    // TODO: Preguntar José cómo redirigir al usuario a otro .html desde el back.
-    /* TODO: Preguntar José si hay que crear una clase User y crearlo desde el método register. Si es así, 
-            preguntar si hay que utilizar los métodos de esa nueva clase para el login (->getName y ->getPassword).*/
-    public $username = $_POST['username'];
-    public $password = $_POST['password'];
-    function login()
+    public $username;
+    public $password;
+
+    public function __construct() {}
+    public function login()
     {
         // Iniciar sesión para poder guardar los datos del usuario.
         // Devuelve un boolean en caso de que el login sea correcto o no.
-        session_start();
 
         // Que el método reciba los datos del usuario y los compare con los datos de la base de datos.
         // Se utilizan variables fijas para probar el login antes de conectarlo con una base de datos.
@@ -22,27 +41,41 @@ class UserController
 
             //$this->admin = true; --> Por default estará en false siempre. Se cambiará a true si el usuario es admin.
             return true;
-            
-        } else if ($this->username == 'user' && $this->password = 'user') {
+        } else if ($this->username == 'user' && $this->password == 'user') {
             $_SESSION['username'] = $this->username;
             $_SESSION['password'] = $this->password;
 
             return true;
         } else {
             echo 'Login failed';
+            header('Location: ../../login.html');
             return false;
         }
     }
 
-    function register()
+    public function register()
     {
         // Que el método reciba los datos del usuario y los guarde en la base de datos.
-        
+        $this->username = $_POST['username'];
+        $this->password = $_POST['password'];
+
+        if ($this->username != '' && $this->password != '') {
+            $_SESSION['username'] = $this->username;
+            $_SESSION['password'] = $this->password;
+
+            echo 'Register success';
+            return true;
+        } else {
+            echo 'Register failed';
+            header('Location: ../../loginnoregr.html');
+            return false;
+        }
     }
-    function logout()
+    public function logout()
     {
         // Un session destroy y no hay más misterio.
         session_destroy();
         echo 'Logout success';
+        header('Location: ../../html.html');
     }
 }
